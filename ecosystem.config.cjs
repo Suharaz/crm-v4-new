01517820b344c982fae1835c06111318c85d3ed1
Suharaz <minesuhara@gmail.com>
2026-04-12 17:02:41 +0700

    fix(deploy): correct PM2 next.js script path + workflow VPS path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
     {
       name: 'crm-web',
       cwd: './apps/web',
-      script: 'node_modules/.bin/next',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start -p 3011',
       instances: 1,
       exec_mode: 'fork',
